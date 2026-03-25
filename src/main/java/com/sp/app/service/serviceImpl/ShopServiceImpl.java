fc99dfcbf3388d76793b3bca8613a25a1e51e524
kimhyuk <4reoil@gmail.com>
2026-03-25 15:29:47 +0900

    [26.03.25] serviceimpl 파일 분리

---
+++ src/main/java/com/sp/app/service/serviceImpl/ShopServiceImpl.java
@@ -0,0 +1,9 @@
+package com.sp.app.service.serviceImpl;
+
+import com.sp.app.service.ShopService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShopServiceImpl implements ShopService {
+
+}
